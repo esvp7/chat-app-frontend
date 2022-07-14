6552b0823ecf0e03a71f706487816cf536af9368
unknown <you@example.com>
2022-07-14 07:09:56 -0400

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import ChannelListContainer from "./components/ChannelListContainer";
+import ChannelContainer from "./components/ChannelContainer";
+import Auth from "./components/Auth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'stream-chat-react/dist/css/index.css';
+import "./App.css";
+
+const cookies = new Cookies();
+
+const apiKey = "c55q6ma2ed2c";
+const client = StreamChat.getInstance(apiKey);
+const authToken = cookies.get("token");
+
+if(authToken) {
+    client.connectUser({
+        id: cookies.get('userId'),
+        name: cookies.get('username'),
+        fullName: cookies.get('fullName'),
+        image: cookies.get('avatarURL'),
+        hashedPassword: cookies.get('hashedPassword'),
+        phoneNumber: cookies.get('phoneNumber'),
+    }, authToken)
+}
+
+const App = () => {
+	const [createType, setCreateType] = useState('');
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+	if (!authToken) return <Auth />
+	return (
+		<div className="app__wrapper">
+		   <Chat client={client} theme="team light">
+		      <ChannelListContainer 
+		            isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
+		        />
+		      <ChannelContainer 
+		            isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}
+                />
+		   </Chat>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TeamMessage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TeamMessage = () => {
+	return (
+		<div className="">
+			TeamMessage
+		</div>
+	);
+}
+
+export default TeamMessage;
